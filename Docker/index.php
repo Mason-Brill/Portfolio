9adf3e45f97d2a4a4678acaebf35a516332b98b7
Mason-Brill <masonjamesbrill@yahoo.com>
2023-11-21 18:43:47 -0500

    added space svg background

--- Docker/index.php
+++ Docker/index.php
@@ -1,23 +1,33 @@
 <!DOCTYPE html>
+<head>
+	<link rel="stylesheet" href="./index.css">
 
-<link rel="stylesheet" href="./index.css">
-<h1 class="intro">Hello, I am Mason Brill</h1>
+	<h1 class="intro">Hello, I am Mason Brill</h1>
+</head>
+<body>
 
-<?php
-$connect = mysqli_connect(
-	'db',
-	'lamp_docker',
-	'password',
-	'lamp_docker'
-);
+	<img class="background" src="background.svg" alt="space"/>
+	<?php
+	$connect = mysqli_connect(
+		'db',
+		'lamp_docker',
+		'password',
+		'lamp_docker'
+	);
 
-$query = 'SELECT * FROM projects';
-$result = mysqli_query($connect, $query);
+	$query = 'SELECT * FROM projects';
+	$result = mysqli_query($connect, $query);
 
-while($record = mysqli_fetch_assoc($result))
-{
-	echo '<h2>'.$record['title'].'</h2>';
-	echo '<h3>'.$record['skills'].'</h3>';
-	echo '<p>'.$record['description'].'</p>';
-}
-?>
+	while($record = mysqli_fetch_assoc($result))
+	{
+		echo '<h2>'.$record['title'].'</h2>';
+		echo '<h3>'.$record['skills'].'</h3>';
+		echo '<p>'.$record['description'].'</p>';
+	}
+	?>
+</body>
+<h1>a</h1>
+<h1>a</h1>
+<h1>a</h1>
+<h1>a</h1>
+<h1>a</h1>
