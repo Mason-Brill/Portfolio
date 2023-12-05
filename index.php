a61ab189de6f9d418e923f1e4797ac4f088f9042
Mason Brill <masonjamesbrill@yahoo.com>
2023-12-04 21:54:01 -0500

    changing index.css

--- index.php
+++ index.php
@@ -107,7 +107,14 @@ if ($result->num_rows > 0) {
             if ($cssClass == 'even-class') {
                 echo '<img src="./images/' . $counter . '.webp" class="even-image"/>';
             }
-            echo '<h2 class="project-title">' . $record['title'] . '</h2>';
+            echo '<h2 class="project-title">'
+					if($record['title'] == "Pin The Bay"){
+						echo '<a href="www.pinthebay.com">'. $record['title'] . '</a>'
+					}
+					else {
+						. $record['title'] . 
+					}
+				'</h2>';
             echo '<h3 class="skills">' . $record['skills'] . '</h3>';
             echo '<p class="desc">' . $record['description'] . '</p>';
 
