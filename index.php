5b6a494b57fe3e631f1caa2b22eff22d3dc70e24
Mason Brill <masonjamesbrill@yahoo.com>
2024-01-24 14:10:42 -0500

    adding new picture

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 	  </div>
 
 	  <!-- picture of me -->
-	  <img src="https://masons-portfolio-ec4043d216a7.herokuapp.com/images/Mason.webp" class="me"/>
+	  <img src="https://masons-portfolio-ec4043d216a7.herokuapp.com/images/Mason.JPG" class="me"/>
 
 	  <!-- contact me container, flexbox -->
 	  <div class="contact-container">
